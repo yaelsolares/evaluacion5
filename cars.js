34a5ca3922cacb8a77c80e3abd72d60df96fe345
yaelsolares <albertosolares1202@gmail.com>
2020-10-05 23:51:17 -0500

    first commit

---
+++ cars.js
@@ -0,0 +1,28 @@
+const cars = [
+    {
+        id: 1,
+        name: 'BMW',
+        model: 'Serie 3',
+        color: 'Azul',
+        year: 2020,
+        price: 350000,
+    },
+    {
+        id: 2,
+        name: 'Ford',
+        model: 'Mustang',
+        color: 'Rojo',
+        year: 2001,
+        price: 250000,
+    },
+    {
+        id: 3,
+        name: 'Nissan',
+        model: 'March',
+        color: 'Gris',
+        year: 2015,
+        price: 100000,
+    },
+];
+
+
