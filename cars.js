9b0007f513c43a534e019eda8a152ef6197384cd
yaelsolares <albertosolares1202@gmail.com>
2020-10-06 03:25:45 -0500

    commit on master

--- cars.js
+++ cars.js
@@ -25,4 +25,60 @@ const cars = [
     },
 ];
 
+let idEditingCar = -1
+
+function printCars() {
+    const tbody = document.getElementById('t-body');
+    tbody.innerHTML = ''
+    cars.forEach ((car) => {
+          tbody.innerHTML += '<tr><td>' + car.name + '</td><td>' + car.model + '</td><td>' + car.color +'</td><td>' + car.year + '</td><td>' + car.price + '</td><td><button class="btn btn-warning" onclick="editCar('+ car.id +')">Editar</button>' + '</td><td><button class="btn btn-danger" onclick="deleteCar('+ car.id +')">Eliminar</button></td></tr>'
+    })
+}
+
+function editCar(identificador) {
+    //alert('¿Editamos el carro' + id + '?')
+    const editingCar = cars.find((car) => car.id === identificador)
+    const inputName = document.getElementById('name');
+    const inputModel = document.getElementById('model');
+    const inputColor = document.getElementById('color');
+    const inputYear = document.getElementById('year');
+    const inputPrice = document.getElementById('price');
+
+    inputName.value = editingCar.name;
+    inputModel.value = editingCar.model;
+    inputColor.value = editingCar.color;
+    inputYear.value = editingCar.year;
+    inputPrice.value = editingCar.price;
+
+    idEditingCar = editingCar.id;
+}
+
+function updateCar() {
+    const newName = document.getElementById('name').value;
+    const newmodel = document.getElementById('model').value;
+    const newColor = document.getElementById('color').value;
+    const newYear = document.getElementById('year').value;
+    const newPrice = document.getElementById('price').value;
+
+    if(idEditingCar === -1) {
+        alert('Selecciona un carro para editar')
+        return;
+    }
+    const idEditingCar = car.find((car) => car.id === idEditingCar)
+    editingCar.name = newName;
+    editingCar.model = newModel;
+    editingCar.color = newColor;
+    editingCar.year = newYear;
+    editingCar.price = newPrice;
+
+    printCars();
+}
+
+function deleteCar(id) {
+    alert('¿Eliminamos el carro' + id + '?')
+}
+
+printCars();
+
+
 
